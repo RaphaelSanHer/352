5cf75d9627abf642c393a5d4ba6f171e835ed7db
RaphaelSanHer <133154630+RaphaelSanHer@users.noreply.github.com>
2023-05-19 01:58:41 -0600

    V2

---
+++ Unidad1/V2TRIFIV/v2trifiv/V2TRIFIV.java
@@ -0,0 +1,30 @@
+
+package v2trifiv;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class V2TRIFIV {
+
+    public static void main(String[] args) {
+        // declarar variables
+        int numero, mult3, mult5;
+        BufferedReader bufEntrada= new BufferedReader (new InputStreamReader (System.in));
+        System.out.println("Ingresa valor");
+        numero = integer.parseInt(bufEntrada.readLine());
+        
+        mult3= numero % 3;
+        mult5= numero % 5;
+        //Salida de datos
+            if(mult3 ==0 && mult5 == ){
+                System.out.println("TRIFIV");
+            } else if (mult3==0){
+                System.out.println("TRI");
+            } else if (mult5==0){
+                System.out.println("Fiv");
+            }
+         if (mult3==0 && mult5==0){
+             System.out.println("Trifiv");
+         }
+    }
+}
+    
+
