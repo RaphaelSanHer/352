7e468ab6f3220a55daee69bac62a62a69bb26a8b
RaphaelSanHer <133154630+RaphaelSanHer@users.noreply.github.com>
2023-05-19 15:16:27 -0600

    Update V2TRIFIV.java

--- Unidad1/V2TRIFIV/v2trifiv/V2TRIFIV.java
+++ Unidad1/V2TRIFIV/v2trifiv/V2TRIFIV.java
@@ -1,10 +1,11 @@
 
 package v2trifiv;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 public class V2TRIFIV {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
         // declarar variables
         int numero, mult3, mult5;
         BufferedReader bufEntrada= new BufferedReader (new InputStreamReader (System.in));
