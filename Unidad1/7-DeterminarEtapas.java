222520ceb25f1909da97dfbef1e63951603370f6
RaphaelSanHer <133154630+RaphaelSanHer@users.noreply.github.com>
2023-06-12 23:42:09 -0600

    edades

---
+++ Unidad1/7-DeterminarEtapas.java
@@ -0,0 +1,45 @@
+
+package pkg7.determinaretapas;
+
+
+import java.util.Scanner;
+public class Edades {
+static int Edad = 0;
+
+        public static void main(String[] args) {
+        Solicitar();
+        Edades();
+    }
+        public static void Solicitar(){
+        
+           Scanner save =new Scanner(System.in);
+           
+           System.out.println("Colcar tu edad:");
+           
+           Edad = save.nextInt();
+    }
+        public static void Edades(){
+        
+        if (Edad<=9 && Edad >=0  )
+        {
+           System.out.println("Eres un infante");  
+        }
+   
+        if (Edad<=14 && Edad>=10)
+        {
+           System.out.println("Eres un niño pequeño");  
+        }
+        if (Edad>=14 && Edad <=29)
+        {
+           System.out.println("Eres un adolecente");  
+        }
+        if (Edad>=30 && Edad <=79)
+        {
+           System.out.println("Adulto mayor");  
+        }    
+        if (Edad>=80)
+        {
+           System.out.println("Abuelito");  
+        }    
+    }
+}
