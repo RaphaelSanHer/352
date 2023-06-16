12b954d3e5a60f73a861fc3b79d3d1301fe180cb
RaphaelSanHer <133154630+RaphaelSanHer@users.noreply.github.com>
2023-06-16 14:18:26 -0600

    Aliniado

--- Unidad1/12-Bandera calificación.java
+++ Unidad1/12-Bandera calificación.java
@@ -2,46 +2,50 @@
 package javaapplication25;
 
 import java.util.Scanner;
-public class JavaApplication25 {
 
+public class BanderaCalificacion {
+    static int calificacion1, calificacion2, calificacion3, calificacion4, calificacion5;
     
     public static void main(String[] args) {
-        
+        pedirCalificaciones();
+        calificacionReprobada(calificacion1, calificacion2, calificacion3, calificacion4, calificacion5);
+    }
+    
+    public static void pedirCalificaciones() {
         Scanner save = new Scanner(System.in);
-        int NNOTAS, NAPROBADOS=0, NREPROBADOS=0;
-        double[] notas;
-        double suma = 0, promedio, max, min;
         
-        System.out.print("¿Cuantas notas colocaras?: ");
-        NNOTAS = save.nextInt();
-        notas = new double[NNOTAS];
-
-        System.out.println("Ingrese un total de: " + NNOTAS + " notas");
-        for(int i=0; i<notas.length; i++){
-            System.out.print("Nota "+ (i+1) + ": ");
-            notas[i] = save.nextDouble();
-        }
-        System.out.println("Las Notas a evaluar: ");
-        for (double nota : notas) {
-            System.out.print(nota + "   ");
-        }
-
-//nota mas alta
-        max = notas[0];
-        for (int i=0; i<notas.length; i++) {
-            if(notas[i] > max){
-                max = notas[i];
-            }
-        }
-        //comprar alumnos reprobados
-        for (double nota : notas) {
-            if(nota >= 6) {
-                NAPROBADOS++;
-            } else {
-                NREPROBADOS++;
-            }
-        }
- System.out.println("Total de alumnos reprobados: " + NREPROBADOS);
-
+        System.out.println("la calificación 1:");
+        calificacio1 = save.nextInt();
+        
+        System.out.println("la calificación 2:");
+        calificacion2 = save.nextInt();
+        
+        System.out.println("la calificación 3:");
+        calificacion3 = save.nextInt();
+        
+        System.out.println(" la calificación 4:");
+        calificacion4 = save.nextInt();
+        
+        System.out.println("la calificación 5:");
+        calificacion5 = save.nextInt();
+    }
+    
+    public static void calificacionReprobada(int c1, int c2, int c3, int c4, int c5) {
+        boolean bandera1,bandera2,bandera3,bandera4,bandera5;
+        
+        bandera1 = (calificacion1 <= 5);
+        System.out.println("Calificación reprobatoria: " + bandera1);
+        
+        bandera2 = (calificacion2 <= 5);
+        System.out.println("Calificación reprobatoria: " + bandera2);
+        
+        bandera3 = (calificacion3 <= 5);
+        System.out.println("Calificación reprobatoria: " + bandera3);
+        
+        bandera4 = (calificacion4 <= 5);
+        System.out.println("Calificación reprobatoria: " + bandera4);
+        
+        bandera5 = (calificacion5 <= 5);
+        System.out.println("Calificación reprobatoria: " + bandera5);
     }
 }
