3d3d4a8d5282c6dbac09bcf4bd7b2e08873164f7
RaphaelSanHer <133154630+RaphaelSanHer@users.noreply.github.com>
2023-06-16 12:18:04 -0600

    acomodar texto

--- Unidad1/11-Banderas.java
+++ Unidad1/11-Banderas.java
@@ -16,13 +16,13 @@ public class JavaApplication24 {
        boolean bandera1; 
        bandera1 =( edadJuan > edadPedro);
 
-       sistema fuera println("Resultado de bandera:" + bandera1);
+       Systen.out.println("Resultado de bandera:" + bandera1);
        
        int EdadLupe = 35;
        boolean bandera2;
-       boolean bandear3:
+       boolean bandera3:
        bandera2 = (edadLupe < edadPedro);
        bandera3 = (bandera1 && bandera2);
-       sistema fuera println ("resltado de bandera 3:" + bandera3);
+       Systen.out.println ("resltado de bandera 3:" + bandera3);
        
        
